646ff78ec269beb2b7a11c146032027523fd7588
mcbien <mcbien1122@gmail.com>
2020-12-05 14:08:37 -0600

    Saving working flask with age dropdown

--- Project_2_flask.py
+++ Project_2_flask.py
@@ -3,14 +3,17 @@
 
 import numpy as np
 import os
+import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify
 from flask import Flask, render_template
+app = Flask(__name__)
 
 #################################################
 # Jennifer Postgres
@@ -28,8 +31,10 @@ pg_user = 'postgres'
 db_name = 'Gamers'
 
 connection_string = f"{pg_user}:bootcampDavid@1942@localhost:5432/{db_name}"
-engine = create_engine(f'postgresql://{connection_string}')
-
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{connection_string}'
+db = SQLAlchemy(app)
+db.init_app(app)
+engine = db.engine
 
 #################################################
 # Sadies Postgres
@@ -40,21 +45,20 @@ engine = create_engine(f'postgresql://{connection_string}')
 # connection_string = f"{pg_user}:Jennifer11@localhost:5432/{db_name}"
 # engine = create_engine(f'postgresql://{connection_string}')
 
-Base = automap_base()
-Base.prepare(engine, reflect=True)
+# Base = automap_base()
+# Base.prepare(engine, reflect=True)
 
-# Save reference to the table
-revenue = Base.classes.revenue
-age = Base.classes.age
-hours = Base.classes.hours
-gender = Base.classes.gender
+# # Save reference to the table
+# revenue = Base.classes.revenue
+# age = Base.classes.age
+# hours = Base.classes.hours
+# gender = Base.classes.gender
 
 # table = Base.classes.wiki_movie
 
 #################################################
 # Flask Setup
 #################################################
-app = Flask(__name__)
 
 
 #################################################
@@ -75,14 +79,15 @@ def revenue_():
     """Return a list of all movie names"""
 
     # Query all gamer data
-    session = Session(engine)
-    results = session.query(revenue).all()
+    # session = Session(engine)
+    df = pd.read_sql_query("SELECT * FROM age", engine)
+    all_names = df.to_dict(orient="list")
 
     # close the session to end the communication with the database
-    session.close()
+    # session.close()
 
     # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    # all_names = list(np.ravel(results))
     print(all_names)
     # return jsonify(all_names)
     return render_template("index.html", all_names=all_names)
