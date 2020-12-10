7cdfbab55da52a5d16f8fd738e59d698bef02924
mcbien <mcbien1122@gmail.com>
2020-12-10 17:46:06 -0600

    Finally figured out what all names is doing

--- Project_2_flask.py
+++ Project_2_flask.py
@@ -37,7 +37,7 @@ db_name = 'Gamers'
 # pg_user = 'postgres'
 # db_name = 'Gamers'
 
-connection_string = f"{pg_user}:Sugar5728865**@localhost:5432/{db_name}"
+connection_string = f"{pg_user}:bootcampDavid@1942@localhost:5432/{db_name}"
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{connection_string}'
 db = SQLAlchemy(app)
 db.init_app(app)
@@ -64,55 +64,25 @@ engine = db.engine
 # Flask Routes
 #################################################
 
-@app.route("/gamerChoice", methods=["POST"])
-def gamerChoice():
-    choice = request.form["id"]
-    print(choice)
-    gamerName = request.form["gamerName"]
-    print(gamerName)
-    genderOption = request.form["optionsRadios"]
-    print(genderOption)
-
-    # do another query to pull data associated with the age range choice
-
- # Query all gamer data
-    session = Session(engine)
-    df = pd.read_sql_query("SELECT * FROM age", engine)
-    all_ages = df.to_dict(orient="list")
-
-    # close the session to end the communication with the database
-    session.close()
-
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-    print(all_ages)
-    if request.method == "POST":
-        # put all your input info into the database
-        # {
-        # 'name':request.form['gamerName'],
-        # 'location':request.form['location']
-        # return jsonify(all_names)
-        return render_template("index.html", all_names=all_ages)
-    console.log(all_ages);
-
 @app.route("/")
 def age_():
     """Return a list of all movie names"""
 
     # Query all gamer data
-    # session = Session(engine)
+    session = Session(engine)
     df = pd.read_sql_query("SELECT * FROM age", engine)
     all_names = df.to_dict(orient="list")
 
     # close the session to end the communication with the database
-    # session.close()
+    session.close()
 
     # Convert list of tuples into normal list
     # all_names = list(np.ravel(results))
     print(all_names)
-    
+
     # return jsonify(all_names)
     return render_template("index.html", all_names=all_names)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/app-placeholder.js
+++ static/js/app-placeholder.js
@@ -5,7 +5,7 @@ d3.select('#buttonId').on('click', function () {
     d3.json('myGamerLocationUrlHere').then(data, function (data) {
         country = data.location.country_code
         d3.select('#locationInput').value(country)
-        
+
 
 
         // do stuff in here
@@ -24,29 +24,29 @@ d3.select('#buttonId').on('click', function () {
 
 // in Plotly, trace refers to an object that contains 1) data to be plotted, and 2) specifications for plotting.
 var trace1 = {
-    x: "age",
-    y: "avg_hours",
+    x: all_names["age"],
+    y: all_names['avg_hours'],
     type: "bar"
-  };
-  
-  // our trace is enclosed within an Array because you will later see that we can have multiple traces for a plot.
-  var data = [trace1];
-  
-  // layout is optional, but contains chart title, axis information, and any other custom layout behavior
-  var layout = {
+};
+
+// our trace is enclosed within an Array because you will later see that we can have multiple traces for a plot.
+var data = [trace1];
+
+// layout is optional, but contains chart title, axis information, and any other custom layout behavior
+var layout = {
     title: "Average Gaming Hours Played per Age Group"
-  };
-  
-  // the first argument below ("plot") refers to the id of the div where the play will be displayed
-  // the second argument refers to our trace
-  // the third argument is optional. It refers to the chart's layout details.
-  Plotly.newPlot("plot", data, layout);
-  
+};
+
+// the first argument below ("plot") refers to the id of the div where the play will be displayed
+// the second argument refers to our trace
+// the third argument is optional. It refers to the chart's layout details.
+Plotly.newPlot("plot", data, layout);
+
 // ####################################################################################################
 
 
 
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
+var margin = { top: 20, right: 20, bottom: 30, left: 50 },
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
@@ -66,41 +66,41 @@ var yAxis = d3.svg.axis()
     .orient("left");
 
 var line = d3.svg.line()
-    .x(function(d) { return x(d.age); })
-    .y(function(d) { return y(d.avg_hours); });
+    .x(function (d) { return x(d.age); })
+    .y(function (d) { return y(d.avg_hours); });
 
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("id"), function( data) {
-  data.forEach(function(d) {
-    d.age = data.age;
-    d.avg_hours = +data.avg_hours;
-  });
+d3.json("id"), function (data) {
+    data.forEach(function (d) {
+        d.age = data.age;
+        d.avg_hours = +data.avg_hours;
+    });
 
-  x.domain(d3.extent(data, function(d) { return d.age; }));
-  y.domain(d3.extent(data, function(d) { return d.avg_hours; }));
+    x.domain(d3.extent(data, function (d) { return d.age; }));
+    y.domain(d3.extent(data, function (d) { return d.avg_hours; }));
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Play count");
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Play count");
 
-  svg.append("path")
-      .datum(data)
-      .attr("class", "line")
-      .attr("d", line);
+    svg.append("path")
+        .datum(data)
+        .attr("class", "line")
+        .attr("d", line);
 };
