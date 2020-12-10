0218fcca4cf4007227e98bcfc25955c433b7e3f8
jddarby <67566019+jddarby1108@users.noreply.github.com>
2020-12-10 16:26:32 -0600

    adding some tests

--- Project_2_flask.py
+++ Project_2_flask.py
@@ -52,16 +52,11 @@ engine = db.engine
 # connection_string = f"{pg_user}:Jennifer11@localhost:5432/{db_name}"
 # engine = create_engine(f'postgresql://{connection_string}')
 
-# Base = automap_base()
-# Base.prepare(engine, reflect=True)
 
 # # Save reference to the table
 # revenue = Base.classes.revenue
-# age = Base.classes.age
-# hours = Base.classes.hours
-# gender = Base.classes.gender
+# console.log(revenue)
 
-# table = Base.classes.wiki_movie
 
 #################################################
 # Flask Setup
@@ -85,23 +80,41 @@ def gamerChoice():
 
  # Query all gamer data
     session = Session(engine)
-    df = pd.read_sql_query("SELECT * FROM age", engine)
-    all_ages = df.to_dict(orient="list")
+    df_age = pd.read_sql_query("SELECT * FROM age", engine)
+    all_names = df_age.to_dict(orient="list")
 
     # close the session to end the communication with the database
     session.close()
 
+    session = Session(engine)
+    df_rev = pd.read_sql_query("SELECT * FROM revenue", engine)
+    all_revenue = df_rev.to_dict(orient="list")
+
+    session.close()
+
     # Convert list of tuples into normal list
     all_names = list(np.ravel(results))
-    print(all_ages)
+    print(all_names)
     if request.method == "POST":
         # put all your input info into the database
         # {
         # 'name':request.form['gamerName'],
         # 'location':request.form['location']
         # return jsonify(all_names)
-        return render_template("index.html", all_names=all_ages)
-    console.log(all_ages);
+        return render_template("index.html", all_names=all_names)
+
+        all_revenue = list(np.ravel(results))
+    print(all_revenue)
+    if request.method == "POST":
+        # put all your input info into the database
+        # {
+        # 'name':request.form['gamerName'],
+        # 'location':request.form['location']
+        # return jsonify(all_names)
+        return render_template("index.html", all_revenue=all_revenue)
+   
+
+    
 
 @app.route("/")
 def age_():
@@ -121,6 +134,7 @@ def age_():
     
     # return jsonify(all_names)
     return render_template("index.html", all_names=all_names)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
